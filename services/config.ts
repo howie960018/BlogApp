1bca4773528bb2f026a181c2374f9c64a1fe208c
howie <newhowie0427@gmail.com>
2025-11-29 13:04:39 +0800

    deploy settings

--- services/config.ts
+++ services/config.ts
@@ -2,4 +2,6 @@
 // 如果您已下載程式碼並啟動了本地 Node.js 伺服器，請將此改為 true。
 export const USE_REAL_BACKEND = true;
 
-export const API_URL = 'http://localhost:5000/api';
+
+// 根據環境判斷 API URL
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
